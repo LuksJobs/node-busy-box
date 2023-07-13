95cb1b4f30a51ffbe09e0297d74ae53abe6b74c0
luksjobs <luksjobs@live.com>
2023-07-13 14:48:40 -0300

    atualizando os estágios da pipeline, adicionando estágio de teste via script bash

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 var express = require('express');
 var app = express();
 
-// Add this to the very top of the first file loaded in your app
-var apm = require('elastic-apm-node').start({
-  serviceName: 'node-busybox',
-  secretToken: 'un1med_elk_apm',
-  serverUrl: 'http://10.10.33.30:8200',
-  environment: 'ambiente dev'
-})
-
 app.get('/', function (req, res) {
   res.send('By three they come. By three, thy way opens. By the blood of the willing, we call thee home. Hail, Daughter of Hatred. Creator of Sanctuary. Hail...Lilith. ლ,ᔑ•ﺪ͟͠•ᔐ.ლ');
 });
