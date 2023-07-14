ae8ce798901af284a0cdb54b9b2523ca73a95394
luksjobs <luksjobs@live.com>
2023-07-14 11:04:19 -0300

    realizando alterações na sintaxe do CI/CD e validando alterações na imagem docker

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('By three they come. By three, thy way opens. By the blood of the willing, we call thee home. Hail, Daughter of Hatred. Creator of Sanctuary. Hail...Lilith. ლ,ᔑ•ﺪ͟͠•ᔐ.ლ');
 });
 
+app.get('/hail', function (req, res) {
+  res.send('Hail to the king ლ,ᔑ•ﺪ͟͠•ᔐ.ლ');
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('aplicação rodando na porta: ' + port);
